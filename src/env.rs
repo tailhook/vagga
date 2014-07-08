5ffe4b01c67355bf9181615af2b095ac68cd568c
Paul Colomiets <paul@colomiets.name>
2014-07-08 20:43:05 +0000

    Finally got working containers

--- src/env.rs
+++ src/env.rs
@@ -1,4 +1,5 @@
 pub struct Environ {
+    pub vagga_command: String,
     pub vagga_path: Path,
     pub vagga_dir: Path,
 }
--- src/mod.rs
+++ src/mod.rs
@@ -1,6 +1,9 @@
 extern crate serialize;
 extern crate collections;
 extern crate debug;
+extern crate libc;
+extern crate green;
+extern crate rustuv;
 
 extern crate quire;
 extern crate argparse;
@@ -12,22 +15,25 @@ use std::io::stdio::stderr;
 
 use self::config::find_config;
 use self::build::{BuildTask, build_container};
+use self::run::{RunTask, run_chroot, run_container};
 use self::env::{Environ};
 
 
 mod config;
 mod build;
+mod run;
 mod env;
-
 
 fn _main() -> int {
     let mut err = stderr();
     let workdir = getcwd();
 
-    let mypath = Path::new(args().get(0).as_slice());
+    let vcmd = args().move_iter().next().unwrap();
+    let mypath = Path::new(vcmd.as_slice());
     let env = Environ {
         vagga_dir: mypath.dir_path(),
         vagga_path: mypath,
+        vagga_command: vcmd.clone(),
     };
 
     let (config, project_root) = match find_config(&workdir) {
@@ -79,6 +85,60 @@ fn _main() -> int {
                 })
             {
                 Ok(()) => {}
+                Err(text) =>  {
+                    err.write_line(text.as_slice()).ok();
+                    return 2;
+                }
+            }
+        }
+        "_run" => {
+            if args.len() < 2 {
+                err.write_line(
+                    "Usage:\n    vagga _run container_name command [args]"
+                ).ok();
+                return 1;
+            }
+            let name = args.get(0);
+            match run_container(RunTask {
+                environ: &env,
+                config: &config,
+                container: name,
+                command: args.slice(1, args.len()),
+                work_dir: &workdir,
+                project_root: &project_root,
+                stderr: &mut err,
+                })
+            {
+                Ok(x) => {
+                    return x;
+                }
+                Err(text) =>  {
+                    err.write_line(text.as_slice()).ok();
+                    return 2;
+                }
+            }
+        }
+        "__chroot_and_run" => {
+            if args.len() < 2 {
+                err.write_line(
+                    "Usage:\n    vagga __chroot_and_run container_name command [args]"
+                ).ok();
+                return 1;
+            }
+            let name = args.get(0);
+            match run_chroot(RunTask {
+                environ: &env,
+                config: &config,
+                container: name,
+                command: args.slice(1, args.len()),
+                work_dir: &workdir,
+                project_root: &project_root,
+                stderr: &mut err,
+                })
+            {
+                Ok(x) => {
+                    return x;
+                }
                 Err(text) =>  {
                     err.write_line(text.as_slice()).ok();
                     return 2;
