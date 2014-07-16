dba40bc6f9ca63b6bf62dd8e426551a9d04e206a
Paul Colomiets <paul@colomiets.name>
2014-07-16 21:37:51 +0000

    Remove warning from nix version backend

--- backends/nix/version.sh
+++ backends/nix/version.sh
@@ -1,5 +1,7 @@
 #!/bin/sh -e
 
+: ${container_name:=work}
+: ${cache_dir:=.vagga/.cache}
 : ${nix_config:=default.nix}
 : ${nix_attribute:=""}
 : ${NIX_PATH:=${nix_path:-${CALLER_NIX_PATH}}}
@@ -8,5 +10,10 @@
 export NIX_PATH NIX_REMOTE
 
 type nix-instantiate
+type test mkdir readlink
 
-nix-instantiate "${nix_config}" --attr "${nix_attribute}"
+test -d $cache_dir || mkdir -p $cache_dir
+# readlink is needed because if we so --add-root we get that path instead real
+readlink $(
+    nix-instantiate "${nix_config}" --attr "${nix_attribute}" \
+    --add-root $cache_dir/$container_name.drv --indirect)
