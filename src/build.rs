913afe1750c9e781c292928ee580a575d2974215
Paul Colomiets <paul@colomiets.name>
2014-07-31 23:24:35 +0000

    Add `vagga_exe` variable for builders

--- src/build.rs
+++ src/build.rs
@@ -251,6 +251,7 @@ pub fn build_container(environ: &Environ, container: &mut Container,
     }
     try!(makedirs(&container_tmp));
 
+    env.push(("vagga_exe".as_bytes(), environ.vagga_exe.container_as_bytes()));
     env.push(("artifacts_dir".as_bytes(), artifacts_dir.as_vec()));
     env.push(("container_hash".as_bytes(), hash.as_bytes()));
     env.push(("container_root".as_bytes(), container_tmp.as_vec()));
