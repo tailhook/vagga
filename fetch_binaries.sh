9a668f2945017be40b4bef2f2a21cdb9ab8b67a2
Paul Colomiets <paul@colomiets.name>
2015-05-18 23:27:40 +0300

    Updated alpine binaries

--- fetch_binaries.sh
+++ fetch_binaries.sh
@@ -1,22 +1,28 @@
 #!/bin/sh -ex
+ALPINE_VERSION=v3.2
+APK_TOOLS=apk-tools-static-2.6.0_rc4-r0.apk
+BUSYBOX=busybox-static-1.23.2-r0.apk
+ALPINE_KEYS=alpine-keys-1.1-r0.apk
+
+
 mkdir alpine 2>/dev/null || true
 cd alpine
 rm MIRRORS.txt 2>/dev/null || true
 wget --no-use-server-timestamp http://nl.alpinelinux.org/alpine/MIRRORS.txt -O MIRRORS.txt
 mirror=$(sort --random-sort MIRRORS.txt | head -n 1)
-wget --no-use-server-timestamp $mirror/v3.1/main/x86_64/apk-tools-static-2.5.0_rc1-r0.apk -O apk-tools-static-2.5.0_rc1-r0.apk
-wget --no-use-server-timestamp $mirror/v3.1/main/x86_64/busybox-static-1.22.1-r15.apk -O busybox-static-1.22.1-r15.apk
-wget --no-use-server-timestamp $mirror/v3.1/main/x86_64/alpine-keys-1.1-r0.apk -O alpine-keys-1.1-r0.apk
+wget --no-use-server-timestamp $mirror/$ALPINE_VERSION/main/x86_64/$APK_TOOLS -O $APK_TOOLS
+wget --no-use-server-timestamp $mirror/$ALPINE_VERSION/main/x86_64/$BUSYBOX -O $BUSYBOX
+wget --no-use-server-timestamp $mirror/$ALPINE_VERSION/main/x86_64/$ALPINE_KEYS -O $ALPINE_KEYS
 
 sha1sum -c <<SHA1SUMS
-24900dd5945e0c3d5bc6ee8ce1b8f3d3e21430d6  apk-tools-static-2.5.0_rc1-r0.apk
-7c7496702df578fcd3e2112d99ffa31e08dcfde7  busybox-static-1.22.1-r15.apk
-2cbad6e762c895bfc20c3d0eb357f5d7e750da45  alpine-keys-1.1-r0.apk
+4e93e25cc41ce44d7039371417d3eddc70b9298f  $APK_TOOLS
+ff581245a7291bb13a642a4a90704829bf6cbbbb  $BUSYBOX
+036260ac2053a02048a57703c62c0730e63b0d79  $ALPINE_KEYS
 SHA1SUMS
 cd ..
 
-tar -xf alpine/apk-tools-static-2.5.0_rc1-r0.apk sbin/apk.static -O > apk
-tar -xf alpine/busybox-static-1.22.1-r15.apk bin/busybox.static -O > busybox
-cp alpine/alpine-keys-1.1-r0.apk alpine-keys.apk
+tar -xf alpine/$APK_TOOLS sbin/apk.static -O > apk
+tar -xf alpine/$BUSYBOX bin/busybox.static -O > busybox
+cp alpine/$ALPINE_KEYS alpine-keys.apk
 
 chmod +x apk busybox
