533baa16587e7b1e25b8dbe11f333f03d23292cb
Paul Colomiets <paul@colomiets.name>
2015-07-12 14:33:41 +0300

    Add forgotten try_msg macro

---
+++ src/macros.rs
@@ -0,0 +1,11 @@
+#[macro_export]
+macro_rules! try_msg {
+    ($op:expr, $message:expr) => (
+        try!(($op)
+             .map_err(|e| format!($message, err=e)))
+    );
+    ($op:expr, $message:expr, $($key:ident=$value:expr)*) => (
+        try!(($op)
+             .map_err(|e| format!($message, err=e, $($key=$value),*)))
+    );
+}
