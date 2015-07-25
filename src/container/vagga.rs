0655f358fcbe112ae42447baf5c90eecae62cc0f
Paul Colomiets <paul@colomiets.name>
2015-07-25 11:49:25 +0300

    Fix container/vagga.rs

--- src/container/vagga.rs
+++ src/container/vagga.rs
@@ -1,14 +1,14 @@
 use std::fs::read_link;
+use std::os::unix::ffi::OsStrExt;
 use std::path::Path;
 
 
 pub fn container_ver(name: &String) -> Result<String, String> {
-    let lnk = try!(read_link(&Path::new("/work/.vagga")
-                             .join(name.as_slice()))
+    let lnk = try!(read_link(&Path::new("/work/.vagga").join(&name))
                    .map_err(|e| format!("Error reading link: {}", e)));
-    let lnkcmp = lnk.str_components().collect::<Vec<Option<&str>>>();
-    if lnkcmp.len() < 3 || lnkcmp[lnkcmp.len()-2].is_none() {
+    let lnkcmp = lnk.iter().collect::<Vec<_>>();
+    if lnkcmp.len() < 3 || lnkcmp[lnkcmp.len()-2].to_str().is_none() {
         return Err(format!("Broken container link"));
     }
-    return Ok(lnkcmp[lnkcmp.len()-2].unwrap().to_string());
+    return Ok(lnkcmp[lnkcmp.len()-2].to_str().unwrap().to_string());
 }
