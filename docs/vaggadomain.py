cf7f918c23c03424382080996516a096b31ce366
Paul Colomiets <paul@colomiets.name>
2015-09-08 23:24:27 +0300

    Add vagga domain to sphinx configuration (for easier cross-linking)

---
+++ docs/vaggadomain.py
@@ -0,0 +1,13 @@
+from domaintools import custom_domain
+
+def setup(app):
+    app.add_domain(custom_domain('VaggaOptions',
+        name  = 'vagga',
+        label = "Vagga Yaml Options",
+
+        elements = dict(
+            opt = dict(
+                objname      = "Yaml Option",
+                indextemplate = "option: %s",
+            ),
+        )))
