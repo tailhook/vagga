a6dfb8648a20fb6b67eb7857b9e50f5e1cf64418
Paul Colomiets <paul@colomiets.name>
2016-05-03 23:32:48 +0300

    Implement a container list (closes #233)

--- src/launcher/list.rs
+++ src/launcher/list.rs
@@ -11,9 +11,13 @@ pub fn print_list(config: &Config, mut args: Vec<String>)
     let mut all = false;
     let mut builtin = false;
     let mut hidden = false;
+    let mut containers = false;
     {
         args.insert(0, String::from("vagga _list"));
         let mut ap = ArgumentParser::new();
+        ap.refer(&mut containers)
+            .add_option(&["--containers"], StoreTrue,
+                "Show containers instead of commands");
         ap.refer(&mut all)
             .add_option(&["-A", "--all"], StoreTrue,
                 "Show all commands");
@@ -28,34 +32,41 @@ pub fn print_list(config: &Config, mut args: Vec<String>)
             Err(x) => return Ok(x),
         }
     }
-    let mut out = stdout();
-    for (k, cmd) in config.commands.iter() {
-        out.write_all(k.as_bytes()).ok();
-        match cmd.description() {
-            Some(ref val) => {
-                if k.len() > 19 {
-                    out.write_all(b"\n                    ").ok();
-                } else {
-                    for _ in k.len()..19 {
+    if containers {
+        for (cname, _) in config.containers.iter() {
+            println!("{}", cname);
+        }
+    } else {
+        let mut out = stdout();
+        for (k, cmd) in config.commands.iter() {
+            out.write_all(k.as_bytes()).ok();
+            match cmd.description() {
+                Some(ref val) => {
+                    if k.len() > 19 {
+                        out.write_all(b"\n                    ").ok();
+                    } else {
+                        for _ in k.len()..19 {
+                            out.write_all(b" ").ok();
+                        }
                         out.write_all(b" ").ok();
                     }
-                    out.write_all(b" ").ok();
+                    out.write_all(val.as_bytes()).ok();
                 }
-                out.write_all(val.as_bytes()).ok();
+                None => {}
             }
-            None => {}
+            out.write_all(b"\n").ok();
         }
-        out.write_all(b"\n").ok();
-    }
 
-    if all || builtin {
-        write!(&mut out, "\
-            _build              Build a container\n\
-            _run                Run arbitrary command, \
-                                optionally building container\n\
-            _clean              Clean containers and build artifacts\n\
-            _list               List of built-in commands\n\
-        ").ok();
+        if all || builtin {
+            // TODO(tailhook) fetch builtins from completion code
+            write!(&mut out, "\
+                _build              Build a container\n\
+                _run                Run arbitrary command, \
+                                    optionally building container\n\
+                _clean              Clean containers and build artifacts\n\
+                _list               List of built-in commands\n\
+            ").ok();
+        }
     }
     return Ok(0);
 }
