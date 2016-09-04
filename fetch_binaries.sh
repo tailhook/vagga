eb03fca5fbdf5bcd5fe54011bc644d67c0f7bfef
Paul Colomiets <paul@colomiets.name>
2016-09-05 02:50:58 +0300

    Upgrade busybox

--- fetch_binaries.sh
+++ fetch_binaries.sh
@@ -1,7 +1,7 @@
 #!/bin/sh -ex
 ALPINE_VERSION=v3.4
 APK_TOOLS=apk-tools-static-2.6.7-r0.apk
-BUSYBOX=busybox-static-1.24.2-r9.apk
+BUSYBOX=busybox-static-1.24.2-r11.apk
 ALPINE_KEYS=alpine-keys-1.1-r0.apk
 
 
@@ -14,14 +14,14 @@ wget --no-use-server-timestamp http://nl.alpinelinux.org/alpine/MIRRORS.txt -O M
 sed -i.bak /lax-noc.com/D MIRRORS.txt
 
 # OS X doesn't have --random-sort
-mirror=$(head -n 1 MIRRORS.txt) 
+mirror=$(head -n 1 MIRRORS.txt)
 wget --no-use-server-timestamp ${mirror}$ALPINE_VERSION/main/x86_64/$APK_TOOLS -O $APK_TOOLS
 wget --no-use-server-timestamp ${mirror}$ALPINE_VERSION/main/x86_64/$BUSYBOX -O $BUSYBOX
 wget --no-use-server-timestamp ${mirror}$ALPINE_VERSION/main/x86_64/$ALPINE_KEYS -O $ALPINE_KEYS
 
 sha1sum -c - <<SHA1SUMS
 eba31757fd5dd94f11475ab45036351ae157d260  $APK_TOOLS
-63e3c39f51cf11b3acb0765609d41e932346126f  $BUSYBOX
+e502c7a648287f21895dbbba88dee68269cbcc45  $BUSYBOX
 43e2920260f598d37fe4b1157cd44f1f2581613f  $ALPINE_KEYS
 SHA1SUMS
 cd ..
