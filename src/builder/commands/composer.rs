159015e913b12303e4153a356539ddcd86c154d8
Livio Ribeiro <livioribeiro@outlook.com>
2017-01-18 13:55:42 -0300

    Automatically link runtime_exe to /usr/bin/php

--- src/builder/commands/composer.rs
+++ src/builder/commands/composer.rs
@@ -1,6 +1,7 @@
 use std::path::{Path, PathBuf};
 use std::fs::{self, File};
 use std::io::Write;
+use std::os::unix;
 
 use unshare::{Command};
 use scan_dir::{self, ScanDir};
@@ -224,10 +225,20 @@ pub fn bootstrap(ctx: &mut Context) -> Result<(), String> {
     try_msg!(Dir::new(COMPOSER_HOME).recursive(true).create(),
         "Error creating composer home dir {d:?}: {err}", d=COMPOSER_HOME);
 
-    let runtime_exe = ctx.composer_settings
-        .runtime_exe
+    let runtime_exe = ctx.composer_settings.runtime_exe
         .clone()
         .unwrap_or(DEFAULT_RUNTIME.to_owned());
+
+    let default_runtime = Path::new("/vagga/root")
+        .join(DEFAULT_RUNTIME.trim_left_matches('/'));
+    // if using a custom runtime, link it to '/usr/bin/php' so that packages expecting the `php`
+    // binary to be on PATH can work correctly
+    if runtime_exe != DEFAULT_RUNTIME && !default_runtime.exists() {
+        try_msg!(
+            unix::fs::symlink(&runtime_exe, &default_runtime),
+            "Error creating symlink '{s}' to '{d}': {err}", s=runtime_exe, d=DEFAULT_RUNTIME
+        );
+    }
 
     let cached_composer = format!("/vagga/root{}/composer.phar", COMPOSER_SELF_CACHE);
     if Path::new(&cached_composer).exists() {
