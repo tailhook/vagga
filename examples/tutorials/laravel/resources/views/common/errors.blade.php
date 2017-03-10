ffab7fdcea25f00479bc5e75f97b0380b398252f
Livio Ribeiro <livioribeiro@outlook.com>
2017-03-10 10:47:28 -0300

    refactoring php tutorial

--- examples/tutorials/laravel/resources/views/common/errors.blade.php
+++
@@ -1,9 +0,0 @@
-@if (count($errors) > 0)
-<div class="alert alert-danger">
-    <ul>
-        @foreach ($errors->all() as $error)
-            <li>{{ $error }}</li>
-        @endforeach
-    </ul>
-</div>
-@endif
--- examples/tutorials/laravel/routes/web.php
+++ examples/tutorials/laravel/routes/web.php
@@ -5,15 +5,12 @@
 | Web Routes
 |--------------------------------------------------------------------------
 |
-| This file is where you may define all of the routes that are handled
-| by your application. Just tell Laravel the URIs it should respond
-| to using a Closure or controller method. Build something great!
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
 |
 */
 
-Route::get('/', 'ArticleController@index');
-Route::resource('/article', 'ArticleController');
-
-Auth::routes();
-
-Route::get('/home', 'HomeController@index');
+Route::get('/', function () {
+    return view('welcome');
+});
