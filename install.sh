6fa128526c4ff298ddf803f699e1d4b427b65dbd
Nikita Uvarov <uv.nikita@gmail.com>
2017-03-12 17:11:13 +0100

    Move bash completion script to completions folder

--- install.sh
+++ install.sh
@@ -5,7 +5,7 @@ PREFIX=${PREFIX:-/usr}
 install -d ${DESTDIR}/etc/bash_completion.d
 install -d ${DESTDIR}${PREFIX}/bin
 install -d ${DESTDIR}${PREFIX}/lib/vagga
-install -m 644 vagga-completion.sh ${DESTDIR}/etc/bash_completion.d/vagga
+install -m 644 completions/bash-completion.sh ${DESTDIR}/etc/bash_completion.d/vagga
 install -m 755 vagga ${DESTDIR}${PREFIX}/lib/vagga/vagga
 install -m 755 apk ${DESTDIR}${PREFIX}/lib/vagga/apk
 install -m 755 busybox ${DESTDIR}${PREFIX}/lib/vagga/busybox
