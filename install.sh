c76c60dfdbbedae0b34849570cac34d9f0bc39dd
Nikita Uvarov <uv.nikita@gmail.com>
2017-03-12 19:39:56 +0100

    Add zsh completion script

--- install.sh
+++ install.sh
@@ -3,9 +3,11 @@ DESTDIR=${DESTDIR:-}
 PREFIX=${PREFIX:-/usr}
 
 install -d ${DESTDIR}/etc/bash_completion.d
+install -d ${DESTDIR}${PREFIX}/share/zsh/site-functions
 install -d ${DESTDIR}${PREFIX}/bin
 install -d ${DESTDIR}${PREFIX}/lib/vagga
 install -m 644 completions/bash-completion.sh ${DESTDIR}/etc/bash_completion.d/vagga
+install -m 644 completions/zsh-completion.sh ${DESTDIR}${PREFIX}/share/zsh/site-functions/_vagga
 install -m 755 vagga ${DESTDIR}${PREFIX}/lib/vagga/vagga
 install -m 755 apk ${DESTDIR}${PREFIX}/lib/vagga/apk
 install -m 755 busybox ${DESTDIR}${PREFIX}/lib/vagga/busybox
